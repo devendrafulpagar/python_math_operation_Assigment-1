47a979336ef7728f8e4e07b81b725bc81ee6c1b2
devendrafulpagar <48725506+devendrafulpagar@users.noreply.github.com>
2025-04-30 01:03:39 +0530

    Add files via upload

---
+++ Assignment 2/Even or Odd.py
@@ -0,0 +1,8 @@
+i = int(input("Enter The Number : "))
+
+if i % 2 == 0:
+    print(i, "is an even number.")
+else:
+    print(i, "is an odd number")
+
+
---
+++ Assignment 2/Sum 1-50  using loop.py
@@ -0,0 +1,6 @@
+total = 0
+
+for num in range(1,51):
+    total = total + num
+
+print("The Sum of numbers from 1 to 50 is :",total)
