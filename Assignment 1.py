01dc29c37ca9445c23919bb66f26f76dd318a663
devendrafulpagar <48725506+devendrafulpagar@users.noreply.github.com>
2025-04-29 15:39:33 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,17 @@
+a=input("Enter The First Number: ")
+a=int(a)
+b=input("Enter The Second Number: ")
+b=int(b)
+c = a + b
+c=int(c)
+c1 = a - b
+c1=int(c1)
+c2 = a * b
+c2=int(c2)
+c3 = a / b
+c3=int(c3)
+
+print('Addition :',c)
+print('Subtraction :',c1)
+print('Multiplication :',c2)
+print('Division :',c3)
